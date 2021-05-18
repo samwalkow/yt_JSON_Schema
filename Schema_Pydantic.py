45c9f243c7bb742cee5cc2febf7b77540d1cae34
Sam Walkow <samwalkow@gmail.com>
2021-05-18 17:06:34 -0400

    Got design classes to work but hard coded

--- Schema_Pydantic.py
+++ Schema_Pydantic.py
@@ -1,13 +1,17 @@
 #%%
 
 from enum import Enum
+from types import new_class
+from numpy import place
 from pydantic import BaseModel, Field, constr
-from typing import Generic, List, Union, Dict, Optional
+from typing import Generic, List, Union, Dict, Optional, Sequence, Tuple
 import pydantic
 from pydantic.main import create_model
 from inspect import getfullargspec
 import yt
 
+
+#%%
 class ytBaseModel(BaseModel):
     _arg_mapping: dict = {}  # mapping from internal yt name to schema name
     _yt_operation: Optional[str]
@@ -15,15 +19,17 @@ class ytBaseModel(BaseModel):
     def _run(self):
         # this method actually executes the yt code
 
+
         # first make sure yt is imported and then get our function handle. This assumes
         # that our class name exists in yt's top level api.
         import yt
-        print(yt.__version__)
-        print(self._yt_operation)
+        #print(yt.__version__)
 
+        print("yt operation:", self._yt_operation)
+    
         funcname = getattr(self, "_yt_operation", type(self).__name__)
         func = getattr(yt, funcname)
-        print(f"pulled func {func}")
+        #print(f"pulled func {func}")
 
         # now we get the arguments for the function:
         # func_spec.args, which lists the named arguments and keyword arguments.
@@ -82,6 +88,7 @@ class ytBaseModel(BaseModel):
             the_args.append(arg_value)
         print(the_args)
         return func(*the_args)
+    
 
 class ytParameter(BaseModel):
     _skip_these = ['comments']
@@ -103,7 +110,6 @@ class Dataset(ytBaseModel):
     filename: str
     name: str = "Data for Science"
     comments: Optional[str] 
-    _grammar: str = "registration"
     _yt_operation: str = "load"
     _arg_mapping: dict = {'fn' : 'filename'}
     
@@ -114,19 +120,28 @@ class Fields(ytParameter):
     # getting an error with unit enabled
     _unit: Optional[str]
     comments: Optional[str]
-    _grammar: str = "selection"
+
+class Sphere(ytBaseModel):
+    # found in the 'selection_data_containers.py' 
+    Center: List[float]
+    Radius: Union[float, Tuple[float, str]]
+    _yt_operation: str = "sphere"
+    _arg_mapping: dict = {'center': 'Center', 'radius': 'Radius'}
+
+# class ShadingEnum(ytParameter):
+#     shading: Union[str]
 
 # class AxisPlot(ytParameter):
 #     axis: str
 #     comments: Optional[str]
 
-class Center(ytParameter):
-    center: str
-    comments: Optional[str]
+# class Center(ytParameter):
+#     center: str
+#     comments: Optional[str]
 
-class Widths(ytParameter):
-    width: str
-    comments: Optional[str]
+# class Widths(ytParameter):
+#     width: str
+#     comments: Optional[str]
 
 # class ColorMap(BaseModel):
 #     # list of pre-determined strings
@@ -153,76 +168,148 @@ class Widths(ytParameter):
 #     dataset: Dataset
 #     data_selection: Union[Fields, Operations]
 
+# look at the data source and selection, objects
+
 class SlicePlot(ytBaseModel):
     Dataset: Dataset
     Field: Fields
     Axis: str
-    CenterPlot: Optional[Center]
-    WidthPlot: Optional[List[Widths]]
+    CenterPlot: Optional[str]
+    WidthPlot: Optional[List[str]]
     Comments: Optional[str]
     _yt_operation: str = "SlicePlot"
     _arg_mapping: dict = {'ds': 'Dataset', 'fields': 'Field',
                           'axis': 'Axis', 'center': 'CenterPlot', 'width': 'WidthPlot'}
-    #Annotation : bool = False
-    # color map - domain specific
-    #ColorMap: str = None
-    #_PlotFunctions: _PlotAttributes
+  
 
 class ProjectionPlot(ytBaseModel):
     Dataset: Dataset
     Field: Fields
-    Axis: str
-    CenterPlot: Center
-    WidthPlot: Optional[List[Widths]]
+    Axis: Union[str, int]
+    # domain stuff here. Can we simplify? Contains operations stuff too
+    CenterPlot: Optional[str]
+    # more confusing design. Can we simplify? This contain field names, units, and widths
+    WidthPlot: Optional[Union[tuple, float]]
+    WeightedField: Optional[Fields]
+    AxesUnit: str
+    # need to sort this design out
+    # might need to be a seperate class since we need to limit the length
+    Origin: Optional[Union[str, Sequence]]
+    #DataSource: Optional[Sphere]
     Comments: Optional[str]
     _yt_operation: str = "ProjectionPlot"
     _arg_mapping: dict = {'ds': 'Dataset', 'fields': 'Field',
-                          'axis': 'Axis', 'center': 'CenterPlot', 'width': 'WidthPlot'}
+                            'axis': 'Axis', 'center': 'CenterPlot',
+                          'weight_field': 'WeightedField', 'axes_unit': 'AxesUnit', 'data_source': 'DataSource'}
 
 class PhasePlot(ytBaseModel):
     Dataset: Dataset
     xField: Fields
     yField: Fields
     zField: Union[Fields, List[Fields]]
+    WeightedField: Optional[Fields]
+    xBins: int
+    yBins: int
+    # different names and explaintions for accumulation and fractional and shading
+    Accumulation: Union[bool, List[bool]]
+    Fractional: bool
+    FigureSize: int
+    FontSize: int
+    # different name? Maybe should be an enum?
+    Shading: str
     Comments: Optional[str]
     _yt_operation: str = "PhasePlot"
-    _arg_mapping: dict = {'data_source': 'Dataset', 'x_field': 'xField', ' y_field': 'yField', 'z_fields': 'zField'}
+    _arg_mapping: dict = {'data_source': 'Dataset', 'x_field': 'xField', 'y_field': 'yField', 'z_fields': 'zField', 'weight_field': 'WeightedField', 'x_bins': 'xBins', 'y_bins': 'yBins', 'accumulation': 'Accumulation', 'fractional': 'Fractional', 'figure_size': 'FigureSize', 'fontsize': 'FontSize',
+    'shading': 'Shading'}
+
+class Visualizations(BaseModel):
+    # use pydantic basemodel
+    SlicePlot: Optional[SlicePlot]
+    ProjectionPlot: Optional[ProjectionPlot]
+    PhasePlot: Optional[PhasePlot]
+    #_yt_operation: str = None
+
+# %%
 
 class ytModel(ytBaseModel):
     '''
     An example for a yt analysis schema using Pydantic
     '''
-    Plot: List[ProjectionPlot]
-    #Load: Dataset
+    #Plot: List[Union[ProjectionPlot, PhasePlot, SlicePlot]]
+    Plot: List[Visualizations]
 
     class Config:
         title = 'yt example'
         underscore_attrs_are_private = True
+
+    # def _no_operation(self):
+    #     att = getattr(self, "Visualization")
+    #     print(att)
     
     def _run(self):
         # for the top level model, we override this. Nested objects will still be recursive!
         att = getattr(self, "Plot")
-        return [p._run() for p in att]
+        print("full att:", att)
+        print()
+        x_list = []
+        # for x in att:
+        #     print("an att:", x)
+        #     if hasattr(x, "_yt_operation"):
+        #         print("this is the att:", x)
+        #         run_att = x._run()
+        #         return run_att
+        #         # print("running att:", run_att)
+        #         # x_list.append(run_att)
+        #         # print("the att list:", x_list)
+        #     else:
+        #         continue
+    #    return x_list
+        for p in att:
+            print("atts:", p)
+            new_att = getattr(p, "SlicePlot")
+            #print("dir:", dir(new_att))
+            print("new att:", new_att)
+            print()
+            
+            return new_att._run()
 
-json_slice = {"Plot": [{"Dataset": {
+        #return [p._run() for p in att]
+
+json_slice = {"Dataset": {
     "filename": "IsolatedGalaxy/galaxy0030/galaxy0030"},
     "Field": {"field": "density"},
-    "Axis": "x"}]}
+    "Axis": "x"}
 
-json_projection = {"Plot": [{"Dataset": {
+json_projection = {"Dataset": {
     "filename": "IsolatedGalaxy/galaxy0030/galaxy0030"},
-    "Field": {"field": "temperature"},
-    "Axis": "x",
-    "CenterPlot": {"center": "c"}}]}
+    "Field": {"field": "density"},
+    "Axis": "x", 
+    "CenterPlot": "c",
+    "WeightedField": {"field": "velocity_magnitude"},
+    "AxesUnit": "cm"}
+    #"DataSource": {"Center": [0.75, 0.5, 0.5], "Radius": 2.0}}
 
-json_phase = {"Plot": [{"Dataset": {
+json_phase = {"Dataset": {
     "filename": "IsolatedGalaxy/galaxy0030/galaxy0030"},
     "xField": {"field": "density"},
-    "yField": {"field" :"density"},
-    "zField": {"field": "velocity"}}]}
+    "yField": {"field" : "temperature"},
+    "zField": {"field": "velocity_magnitude"},
+    "WeightedField": {"field": "density"},
+    "xBins": 100,
+    "yBins": 100,
+    "Accumulation": False,
+    "Fractional": True,
+    "FigureSize": 8,
+    "FontSize": 16,
+    "Shading": "nearest"}
 
-analysis_model = ytModel(Plot=json_projection["Plot"])
-print(analysis_model)
+# %%
+
+analysis_model = ytModel(Plot = [{"SlicePlot": json_slice}])
+#analysis_model = ytModel(Plot= [json_projection, json_slice])
+
+print("the model:", analysis_model)
+print(type(analysis_model))
 
 # print("Instance Example:")
 # print(analysis_model.json(indent=2))
@@ -230,19 +317,38 @@ print(analysis_model)
 # print("Schema Example:")
 # print(analysis_model.schema_json(indent=2))
 # print()
+
+
+# %%
+
+# def show_plots(schema):
+#     result = schema._run()
+#     print(result)
+#     for output in range(len(tuple(result))):
+#         print("each output:", result[output])
+#         result[output].show()
+
+# print(show_plots(analysis_model))
+
 result = analysis_model._run()
-result[0].show()
+print(result.show())
 
-# yt_dynamic = create_model("Dynamic_yt_model", dataset=(str, "file.txt"),
-#     FieldList = (list, ["density", "temperature"]), axis=(str, "x"))
+# %%
 
-# print(yt_dynamic.schema_json(indent=2))
-
-# with open("pydantic_schema_file.json", "w") as file:
-#     file.write(analysis_model.schema_json(indent=2))
+with open("pydantic_schema_file.json", "w") as file:
+    file.write(analysis_model.schema_json(indent=2))
 
 
 # %%
 import yt
-yt.PhasePlot?
+import inspect
+
+ds = yt.load("IsolatedGalaxy/galaxy0030/galaxy0030")
+sp = ds.sphere([0.5, 0.5, 0.5], 0.1)
+
+method_list = inspect.getmembers(sp, predicate=inspect.getmembers)
+ 
+print(method_list)
+
+
 # %%
