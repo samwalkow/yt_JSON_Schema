3bcd422c262d79c72d0d53568ee30b221ce6d799
Sam Walkow <samwalkow@gmail.com>
2021-06-14 15:07:36 -0400

    Split Schema_Pydantic File into five files

--- ModifySchema.py
+++ ModifySchema.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Dict, Type, Any
 class Dataset(BaseModel):
     dataset_name: str = "Density"
     data_file: str = "sam.txt"
+    grammar: str = "registration"
 
 class ytCreateModel(BaseModel):
     data_out: Optional[bool]
@@ -13,8 +14,8 @@ class ytCreateModel(BaseModel):
 
     class Config:
         @staticmethod
-        def schema_extra(schema: Dict[str, Any], model:Type['ytCreateModel'])->    None:
-                for prop in schema.get('definitions', {}).values():
-                    prop.pop('title', None)
+        def schema_extra(schema: Dict[str, Any], model:Type['ytCreateModel'])->None:
+            for s in schema.get("properties", {}).values():
+                print(s)
 
 print(ytCreateModel.schema_json(indent=2))
